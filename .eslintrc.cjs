8d2e4c78b969a4af78d61aecff7ddec510953b04
Binish <binishjoshi@proton.me>
2023-09-29 12:25:13 +0545

    ran prettier on all fiels

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,10 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  extends: ['@remix-run/eslint-config', '@remix-run/eslint-config/node'],
   rules: {
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      { vars: "all", args: "after-used", ignoreRestSiblings: false },
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
     ],
   },
 };

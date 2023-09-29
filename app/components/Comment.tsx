873502989f334356fa7d4a29b2343be8741d158a
Binish <binishjoshi@proton.me>
2023-09-29 13:58:28 +0545

    added comment component  need to implement read more comment

---
+++ app/components/Comment.tsx
@@ -0,0 +1,32 @@
+import type { Comment } from '../root';
+
+export default function Comment({ comment }: { comment: Comment }) {
+  return (
+    <div className='flex p-3 border-black border-2 rounded'>
+      <div className='mr-5'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          className='icon icon-tabler icon-tabler-user'
+          width='80'
+          height='80'
+          viewBox='0 0 24 24'
+          strokeWidth='2'
+          stroke='currentColor'
+          fill='none'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        >
+          <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+          <path d='M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0'></path>
+          <path d='M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2'></path>
+        </svg>
+      </div>
+
+      <div>
+        <p className='text-2xl'>{comment.username}</p>
+        <span className='text-xs'>{comment.time}</span>
+        <p className='text-base whitespace-pre-line'>{comment.content}</p>
+      </div>
+    </div>
+  );
+}
--- app/root.tsx
+++ app/root.tsx
@@ -1,17 +1,36 @@
-import { cssBundleHref } from '@remix-run/css-bundle';
 import type { LinksFunction } from '@remix-run/node';
 import {
   Links,
   LiveReload,
   Meta,
-  Outlet,
   Scripts,
   ScrollRestoration,
 } from '@remix-run/react';
+import Comment from './components/Comment';
 
-export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
+import styles from './tailwind.css';
+
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }];
+
+const comments = [
+  {
+    username: 'grumpy',
+    content: `I lean towards Remix. I've been a big fan of the React router (Remix) authors for a long time.
+
+      I like Next, but don't like how Vercel tries to lock you into their ecosystem, IMO.
+      
+      The newer features of Next are pretty much straight out of Remix. That's not a bad thing necessarily, but I think it's telling that Remix has features worth stealing despite Next being around much longer.
+      `,
+    time: '2023-06-23 18:36:33',
+  },
+  {
+    username: 'denden',
+    content: `I like the structure of Remix. But how small its community is is concerning. I'm afraid I'll hit some roadblock and won't be able to get help.`,
+    time: '2023-06-23 18:59:01',
+  },
 ];
+
+export type Comment = (typeof comments)[0];
 
 export default function App() {
   return (
@@ -22,8 +41,13 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body>
-        <Outlet />
+      <body className='p-10 font-serif'>
+        <h3 className='text-5xl mb-10'>Comments</h3>
+        <div className='flex flex-col gap-5'>
+          {comments.map((comment) => (
+            <Comment key={comment.username} comment={comment} />
+          ))}
+        </div>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
